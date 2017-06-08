bd498f3269c3a8820bf64c857fb719b86e286977
Cesar Cotiz <cotizcesar@gmail.com>
2017-06-08 18:21:37 -0500

    Agregado HTML y CSS con Bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list), #Viene el llamado desde mysite/urls.py > blog/urls.py y con views.post_list enviamos hasta views.py en la funcion post_list
+    url(r'^$', views.post_list, name='post_list'), #Viene el llamado desde mysite/urls.py > blog/urls.py y con views.post_list enviamos hasta views.py en la funcion post_list
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail,  name='post_detail'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date') # Nueva variable Posts filtrando todos los objetos de Post por published_date.
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # Nueva variable Posts filtrando todos los objetos de Post por published_date.
     return render(request, 'blog/post_list.html', {'posts': posts}) # Desde mysite/urls.py > blog/urls.py > hasta blog/views.py: Me dice esta funcion que cuando reciba un request me va a enviar a un template en especifico dentro de blog/template/blog/post_list.html, {'posts':posts} significa que va renderizar lo que esta en la variable posts (eso incluye el QuerySet)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
