d3dbd552415dd0120a586e9df3e7aca77d5e4b0c
Cesar Cotiz <cotizcesar@gmail.com>
2017-06-08 15:53:16 -0500

    Acomodado el QuerySet

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) # Desde mysite/urls.py > blog/urls.py > hasta blog/views.py: Me dice esta funcion que cuando reciba un request me va a enviar a un template en especifico dentro de blog/template/blog/post_list.html, lo que esta en {} no se que significa.
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date') # Nueva variable Posts filtrando todos los objetos de Post por published_date.
+    return render(request, 'blog/post_list.html', {'posts': posts}) # Desde mysite/urls.py > blog/urls.py > hasta blog/views.py: Me dice esta funcion que cuando reciba un request me va a enviar a un template en especifico dentro de blog/template/blog/post_list.html, {'posts':posts} significa que va renderizar lo que esta en la variable posts (eso incluye el QuerySet)
