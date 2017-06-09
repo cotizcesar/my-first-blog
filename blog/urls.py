57ab07bb626e777175776a551ab562c7179687ea
Cesar Cotiz <cotizcesar@gmail.com>
2017-06-09 13:03:25 -0500

    Muchos cambios, ya no se que tanto hice.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #Viene el llamado desde mysite/urls.py > blog/urls.py y con views.post_list enviamos hasta views.py en la funcion post_list
     url(r'^post/(?P<pk>\d+)/$', views.post_detail,  name='post_detail'),
-    
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
